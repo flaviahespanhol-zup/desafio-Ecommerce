1de1747ec999008a3fe5c3bc1fd4052591f5cb61
Flavia Hespanhol <flavia.hespanhol@zup.com.br>
2025-01-29 15:56:22 -0300

    feat: implementa a ProductEntity no pacote models

---
+++ src/main/java/desafio/ecommerce/models/ProductEntity.java
@@ -0,0 +1,50 @@
+package desafio.ecommerce.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    double price;
+    int stock;
+
+    ProductEntity() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
