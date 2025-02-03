453f3e199b603e88ef535c91346d58b7dc1a7a6f
Flavia Hespanhol <flavia.hespanhol@zup.com.br>
2025-02-03 15:28:53 -0300

    feat: adiciona método findByName na productRepository

--- src/main/java/desafio/ecommerce/repositories/ProductRepository.java
+++ src/main/java/desafio/ecommerce/repositories/ProductRepository.java
@@ -1,7 +1,11 @@
 package desafio.ecommerce.repositories;
 
+import desafio.ecommerce.models.ClientEntity;
 import desafio.ecommerce.models.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    Optional<ProductEntity> findByName(String productName);
 }
