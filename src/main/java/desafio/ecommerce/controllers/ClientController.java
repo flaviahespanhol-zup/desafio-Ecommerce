d9f947d70974f96a9ead7bd275f7747cc7dd383d
Flavia Hespanhol <flavia.hespanhol@zup.com.br>
2025-02-04 13:02:07 -0300

    feat: implementa na ClientController validações para quando for atualizar dados de um cliente

--- src/main/java/desafio/ecommerce/controllers/ClientController.java
+++ src/main/java/desafio/ecommerce/controllers/ClientController.java
@@ -4,7 +4,12 @@ import desafio.ecommerce.dtos.ClientDTO;
 import desafio.ecommerce.dtos.PostClientDTO;
 import desafio.ecommerce.exceptions.ClientNotFoundException;
 import desafio.ecommerce.services.ClientService;
+import jakarta.persistence.Column;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,8 +38,13 @@ public class ClientController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ClientDTO> updateClientFactoryController(@PathVariable Long id,
-                                                                   @RequestBody PostClientDTO clientUpdated)
+    public ResponseEntity<ClientDTO> updateClientFactoryController(
+            @NotNull(message = "O nome não pode estar vazio")
+            @NotEmpty(message = "O nome não pode ser nulo")
+            @CPF(message = "CPF inválido")
+            @Email(message = "O email deve ser um endereço de email válido!")
+            @Valid @PathVariable Long id,
+            @RequestBody PostClientDTO clientUpdated)
             throws ClientNotFoundException {
         ClientDTO updateClient = clientService.updateClientFactoryService(id, clientUpdated);
         return ResponseEntity.status(HttpStatus.OK).body(updateClient);
